c173d980481550aa9b1d0ce148dd59d09e7f874d
dapod93 <davidliois50@gmail.com>
2025-06-21 11:01:51 +0700

    feat: generated ent codes
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- database/ent/schema/user.go
+++ database/ent/schema/user.go
@@ -1,18 +1,22 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
 
-// User holds the schema definition for the User entity.
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
 type User struct {
 	ent.Schema
 }
 
-// Fields of the User.
 func (User) Fields() []ent.Field {
-	return nil
-}
-
-// Edges of the User.
-func (User) Edges() []ent.Edge {
-	return nil
+	return []ent.Field{
+		field.Int("id"),
+		field.String("email").NotEmpty().Unique(),
+		field.String("first_name").NotEmpty(),
+		field.String("last_name").NotEmpty(),
+		field.Time("created_at").Default(time.Now().UTC()).Immutable(),
+	}
 }
--- gen.go
+++ gen.go
@@ -2,3 +2,5 @@ package main
 
 //go:generate buf format -w
 //go:generate buf generate
+
+//go:generate go run -mod=mod entgo.io/ent/cmd/ent generate --feature sql/versioned-migration --target ./database/ent ./database/ent/schema
